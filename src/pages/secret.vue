363eca7185080575a8c95efd2392dbbbf76dcabc
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-27 16:49:37 -0500

    corrected images

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,10 +1,13 @@
 <template>
 <h1>Photo Gallery</h1>
-  <img src="../assets/bghome1.jpg" alt="Hello BG"> 
-  <img src="../assets/bghome2.jpg" alt="Hello BG"> 
-    <img src="../assets/bghome3(a).jpg" alt="Hello BG"> 
-    <img src="../assets/bghome3(b).jpg" alt="Hello BG"> 
-    <img src="../assets/bghome3(c).jpg" alt="Hello BG"> 
-    <img src="../assets/bghome4(a).jpg" alt="Hello BG">
-    <img src="../assets/bghome4(b).jpg" alt="Hello BG">
+<div>
+  <img src="../assets/bghome1.jpg"> 
+  <img src="../assets/bghome2.jpg"> 
+    <img src="../assets/bghome3(a).jpg"> 
+    <img src="../assets/bghome3(b).jpg"> 
+    <img src="../assets/bghome3(c).jpg"> 
+    <img src="../assets/bghome4(a).jpg">
+    <img src="../assets/bghome4(b).jpg">
+</div>
+  
 </template>
