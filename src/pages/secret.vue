89ae26d3df5ef38c7bd94e35fd700d1576a45c56
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-27 18:07:23 -0500

    change image suffix

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,13 +1,13 @@
 <template>
 <h1>Photo Gallery</h1>
 <div>
-  <img src="../assets/bghome1.jpg"> 
-  <img src="../assets/bghome2.jpg"> 
-    <img src="../assets/bghome3(a).jpg"> 
-    <img src="../assets/bghome3(b).jpg"> 
-    <img src="../assets/bghome3(c).jpg"> 
-    <img src="../assets/bghome4(a).jpg">
-    <img src="../assets/bghome4(b).jpg">
+  <img src="../assets/bgone.png"> 
+  <img src="../assets/bgtwo.png"> 
+    <img src="../assets/bgthree.png"> 
+    <img src="../assets/bgfour.png"> 
+    <img src="../assets/bgfive.png"> 
+    <img src="../assets/bgsix.png">
+    <img src="../assets/bgseven.png">
 </div>
   
 </template>
