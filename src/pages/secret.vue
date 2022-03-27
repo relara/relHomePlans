0279a55842785c4f8baaca1eed79c315daa2fe10
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-27 18:14:50 -0500

    remove jpg images

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,13 +1,7 @@
 <template>
 <h1>Photo Gallery</h1>
 <div>
-  <img src="../assets/bgone.png"> 
-  <img src="../assets/bgtwo.png"> 
-    <img src="../assets/bgthree.png"> 
-    <img src="../assets/bgfour.png"> 
-    <img src="../assets/bgfive.png"> 
-    <img src="../assets/bgsix.png">
-    <img src="../assets/bgseven.png">
+  
 </div>
   
 </template>
