b56c15d8fd4dd970a00aa1caccc0962b4b9b6ec6
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-30 21:23:44 -0500

    basic setup

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,7 +1,31 @@
 <template>
 <h1>Photo Gallery</h1>
-<div>
-  
-</div>
-  
+<div class="mt-16">
+    <ul class="grid grid-cols-2 gap-12">
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgone.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgtwo.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgthree.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgfour.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgfive.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgsix.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgseven.png" alt="Hello BG">
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-2xl">
+            <img class="h-64" src="../assets/bgeight.jpg" alt="Hello BG">
+        </div>
+    </ul>
+</div>  
 </template>
