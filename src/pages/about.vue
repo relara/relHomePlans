24579f55d198b81fdb19ac61fcc6081d1d4c4fd5
relara <relvira_7232@stu.southtexascollege.edu>
2022-04-03 17:51:44 -0500

    added click to view

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -5,14 +5,17 @@
     <div class="flex items-center justify-center overflow-hidden bg-gray-200 rounded-lg shadow-2xl">
        <img class="h-64" src="../assets/bgabout.png" alt="Hello BG">
        <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-           <input name="search" type="text" class="p-2 border-2 rounded-lg" placeholder="Search" v-model="search" />
-           <span class="text-xs text-center text-red-500">{{ search }}</span>
-           <input name="password" type="password" class="p-2 border-2 rounded-lg" placeholder="Square Footage" v-model="password" />
-           <span class="text-xs text-center text-red-500">{{ passwordError }}</span>
-           <div class="flex space-x-2">
-             <button type="submit" @submit.prevent="logginIn" class="w-1/2 py-2 text-yellow-200 bg-yellow-600 rounded-lg">Search</button>  
-           </div>
-        </form>    
+        </form>
     </div>
   </div>
+           <div class="p-4 bg-yellow-300 border-2 rounded-lg shadow-2xl cursor-pointer"> 
+            <h1>Click to View</h1>
+            <h1>1040 sq. ft.</h1>
+            <h1>Floor Plan</h1>
+          </div> 
+          <div class="p-4 bg-yellow-300 border-2 rounded-lg shadow-2xl cursor-pointer"> 
+            <h1>Click to View</h1>
+            <h1>2400 sq. ft.</h1>
+            <h1>Floor Plan</h1>
+          </div>  
 </template>
