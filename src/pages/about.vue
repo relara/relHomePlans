303a627f5a6d29704982dc67415fb0f406674be3
relara <relvira_7232@stu.southtexascollege.edu>
2022-05-01 09:50:10 -0500

    layout api done

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,21 +1,24 @@
 <script setup>
 </script>
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen-nonav">
+<div>
+  <input type="text" placeholder="Search Project" class="w-full py-4 mt-16 text-xl text-center rounded-full"/>
+</div>
+  <div class="flex flex-col items-center py-6">
     <div class="flex items-center justify-center overflow-hidden bg-gray-200 rounded-lg shadow-2xl">
        <img class="h-64" src="../assets/bgabout.png" alt="Hello BG">
-       <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
-        </form>
     </div>
   </div>
-           <div class="p-4 bg-yellow-300 border-2 rounded-lg shadow-2xl cursor-pointer"> 
-            <h1>Click to View</h1>
-            <h1>1040 sq. ft.</h1>
-            <h1>Floor Plan</h1>
-          </div> 
-          <div class="p-4 bg-yellow-300 border-2 rounded-lg shadow-2xl cursor-pointer"> 
-            <h1>Click to View</h1>
-            <h1>2400 sq. ft.</h1>
-            <h1>Floor Plan</h1>
+           <div>
+             <ul class="grid grid-cols-2 gap-6">
+               <div class="p-4 rounded-lg shadow-2xl bg-slate-200"><h1>Click to View</h1><h1>1040 sq. ft.</h1>
+               <h1>Floor Plan</h1>
+               </div>
+               <div class="p-4 rounded-lg shadow-2xl bg-slate-200">  
+               <h1>Click to  View</h1>
+               <h1>2400 sq. ft.</h1>
+               <h1>Floor Plan</h1>
+               </div> 
+            </ul> 
           </div>  
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,10 @@ import useAuth from '../composable/useAuth';
 const { isAuthenticated, user } = useAuth();
 </script>
 <template>
-<div class="text-lg font-bold"><h1>Full Service Construction Company from start to finish, design, slab to ceiling</h1></div>
 <div class="flex flex-col items-center justify-center space-y-12 min-h-screen-nonav">
     <h2 v-if="!isAuthenticated" class="text-6xl tracking-tighter font-extralight">Home Page</h2>
     <h2 v-else class="text-6xl tracking-tighter font-extralight">Welcome {{ user }}</h2>
   <img src="../assets/bgindex.png" alt="Hello BG">  
 </div>
+<div class="text-2xl font-bold bg-gray-200"><h1>Full Service Construction Company from start to finish, design, slab to ceiling</h1></div>
 </template>
