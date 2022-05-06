778286fbd673c211ce92c453105628844e6a027a
relara <relvira_7232@stu.southtexascollege.edu>
2022-05-05 20:55:02 -0500

    added chat done

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 //import { getAnalytics } from "firebase/analytics";
 
 import { firebaseConfig } from "../firebaseConfig";
@@ -7,3 +8,4 @@ import { firebaseConfig } from "../firebaseConfig";
 export const firebaseApp = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 export const firebaseAuth = getAuth(firebaseApp);
+export const db = getFirestore();
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,4 +1,37 @@
+<script setup>
+import { onUnmounted, ref } from 'vue'
+import useChat from '../composable/useChat'
+import useAuth from '../composable/useAuth'
+
+const { messages, unsubscribe, sendMessage } = useChat()
+const { user } = useAuth()
+
+const newMessage = ref('')
+
+const send = () => {
+    sendMessage(newMessage.value)
+    newMessage.value = ''
+}
+
+onUnmounted(() => {
+    unsubscribe()
+})
+</script>
 <template>
+<h1 class="mt-4 text-2xl font-thin tracking-tighter text-center">Cool Chat</h1>
+<div class="min-h-[200px] w-full mt-4 rounded-lg shadow-2xl flex flex-col justify-between">
+    <ul class="p-4 space-y-4">
+        <li v-for="message in messages" :key="message.id">
+            <div class="flex justify-between px-2 py-2 rounded-lg" :class="user === message.author ? 'bg-green-200' : 'bg-gray-200'">
+                <span>{{ message.text }}</span>
+                <span>by {{ message.author }}</span>
+            </div>
+        </li>
+    </ul>
+    <div>
+        <input class="w-full p-2 rounded-lg focus:outline-none focus:bg-gray-400" type="text" placeholder="Type a message..." v-model="newMessage" @change="send"/>
+    </div>
+</div>
 <h1>Photo Gallery</h1>
 <div class="mt-16">
     <ul class="grid grid-cols-2 gap-12">
